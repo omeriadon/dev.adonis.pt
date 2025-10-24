c9b71bd34b59a1f80cef68c89f2622b3c89c0843
omeriadon <adon.omeri@student.education.wa.edu.au>
2025-10-24 21:11:28 +0800

    Clean up

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,20 @@
+export default function Navbar() {
+	return (
+		<nav className="navbar">
+			<ul className="nav-list">
+				<li>
+					<a href="#">Home</a>
+				</li>
+				<li>
+					<a href="#">About</a>
+				</li>
+				<li>
+					<a href="#">Services</a>
+				</li>
+				<li>
+					<a href="#">Contact</a>
+				</li>
+			</ul>
+		</nav>
+	);
+}
