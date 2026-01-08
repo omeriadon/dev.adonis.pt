5f4e5e47b4a3af736b68d10814c0b91839b3a1c7
Adon Omeri <adon.omeri@student.education.wa.edu.au>
2026-01-08 14:01:26 +0800

    fixed some hover animations etc

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,51 +2,53 @@
 
 import { useTheme } from "./ThemeProvider";
 import styles from "./Footer.module.css";
-import { forwardRef } from "react";
+import { forwardRef, type HTMLAttributes } from "react";
 
-const Footer = forwardRef<HTMLElement>(({ ...props }, ref) => {
-	const { theme, toggleTheme } = useTheme();
+const Footer = forwardRef<HTMLElement, HTMLAttributes<HTMLElement>>(
+	({ ...props }, ref) => {
+		const { theme, toggleTheme } = useTheme();
 
-	const colors = [
-		"#d59d41",
-		"#3d1909",
-		"#753888",
-		"#d0dfe0",
-		"#da3d46",
-		"#63d6a1",
-		"#e89163",
-		"#455b82",
-		"#101a8d",
-		"#000000",
-		"#332923",
-		"#323332",
-		"#b79f72",
-	];
+		const colors = [
+			"#d59d41",
+			"#3d1909",
+			"#753888",
+			"#d0dfe0",
+			"#da3d46",
+			"#63d6a1",
+			"#e89163",
+			"#455b82",
+			"#101a8d",
+			"#000000",
+			"#332923",
+			"#323332",
+			"#b79f72",
+		];
 
-	return (
-		<footer ref={ref} className={styles.footer} role="contentinfo" {...props}>
-			<div className={styles.innerContent}>
-				<p className="noSelect">Adon Omeri</p>
-				<button
-					type="button"
-					onClick={toggleTheme}
-					title={`Switch to ${theme === "dark" ? "light" : "dark"} theme`}
-				>
-					Switch theme: {theme}
-				</button>
-			</div>
-			<div style={{ display: "flex", gap: "0px", margin: "0 -16px" }}>
-				{colors.map((color, i) => (
-					<div
-						key={i}
-						style={{ backgroundColor: color, flex: 1, height: "5px" }}
-						suppressHydrationWarning
-					/>
-				))}
-			</div>
-		</footer>
-	);
-});
+		return (
+			<footer ref={ref} className={styles.footer} role="contentinfo" {...props}>
+				<div className={styles.innerContent}>
+					<p className="noSelect">Adon Omeri</p>
+					<button
+						type="button"
+						onClick={toggleTheme}
+						title={`Switch to ${theme === "dark" ? "light" : "dark"} theme`}
+					>
+						Switch theme: {theme}
+					</button>
+				</div>
+				<div style={{ display: "flex", gap: "0px", margin: "0 -16px" }}>
+					{colors.map((color, i) => (
+						<div
+							key={i}
+							style={{ backgroundColor: color, flex: 1, height: "5px" }}
+							suppressHydrationWarning
+						/>
+					))}
+				</div>
+			</footer>
+		);
+	}
+);
 
 Footer.displayName = "Footer";
 
