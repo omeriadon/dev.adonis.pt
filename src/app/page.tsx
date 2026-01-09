bb1a8ff4af10c2a87afd74913dafc12328d53b77
Adon Omeri <adon.omeri@student.education.wa.edu.au>
2026-01-09 08:56:28 +0800

    a few updates to hover effects and the home screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { TextEffect } from "@/components/motion-primitives/text-effect";
 import { useState, useEffect } from "react";
+import Link from "next/link";
 
 const text1 = "Student developer.";
 const text2 = "Specializing in Swift, SwiftUI, and React.";
@@ -21,11 +22,13 @@ export default function Home() {
 	const [show1, setShow1] = useState(false);
 	const [show2, setShow2] = useState(false);
 	const [show3, setShow3] = useState(false);
+	const [show4, setShow4] = useState(false);
 
 	useEffect(() => {
 		setTimeout(() => setShow1(true), 2000);
 		setTimeout(() => setShow2(true), 5000);
 		setTimeout(() => setShow3(true), 10000);
+		setTimeout(() => setShow4(true), 17000);
 	}, []);
 
 	return (
@@ -44,6 +47,16 @@ export default function Home() {
 				<TextEffect {...sharedProps} className="homeSubtitle">
 					{text3}
 				</TextEffect>
+			)}
+			{show4 && (
+				<div className="homeButtonsGrid">
+					<Link href="/projects" className="homeButton">
+						View Projects
+					</Link>
+					<Link href="/contact" className="homeButton">
+						Get in Touch
+					</Link>
+				</div>
 			)}
 		</div>
 	);
